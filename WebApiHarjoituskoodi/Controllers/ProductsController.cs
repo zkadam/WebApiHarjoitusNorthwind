0eee3c3ff81a6c60f98749c20e5c864ae4ab7d58
zkadam <adam.zakar@student.careeria.fi>
2020-08-28 10:16:58 +0300

    products GET

---
+++ WebApiHarjoituskoodi/Controllers/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiHarjoituskoodi.Models;
+
+namespace WebApiHarjoituskoodi.Controllers
+{
+    [Route("nw/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        northwindContext db = new northwindContext();
+
+        [HttpGet]
+        [Route("")]
+        public List<Products> GetAllProducts()
+        {
+            northwindContext db = new northwindContext();
+            List<Products> products = db.Products.ToList();
+            db.Dispose();
+            return products;
+        }
+    }
+}
