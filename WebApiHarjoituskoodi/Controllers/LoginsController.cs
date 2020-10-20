433b7aff2ce345fa38830600c919b32f62f17820
zkadam <adam.zakar@student.careeria.fi>
2020-10-20 16:20:57 +0300

    logins get lisätty

---
+++ WebApiHarjoituskoodi/Controllers/LoginsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiHarjoituskoodi.Models;
+
+namespace WebApiHarjoituskoodi.Controllers
+{
+    [Route("nw/[controller]")]
+    [ApiController]
+    public class LoginsController : ControllerBase
+    {
+        northwindContext db = new northwindContext();
+        //-------------------get all northwind employees
+        [HttpGet]
+        [Route("")]
+        public List<Logins> GetAllLogins()
+        {
+            List<Logins> logins = db.Logins.ToList();
+            foreach (var login in logins)
+            {
+                login.PassWord = "*****";
+            }
+            db.Dispose();
+            return logins;
+        }
+    }
+}
