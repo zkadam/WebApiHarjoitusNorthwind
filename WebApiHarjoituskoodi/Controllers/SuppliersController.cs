253f11889afcc8408192b0f3d8f3eed4d02bb871
zkadam <adam.zakar@student.careeria.fi>
2020-12-18 09:10:59 +0200

    Suppliers GET lisätty react-native dropdownia varten

---
+++ WebApiHarjoituskoodi/Controllers/SuppliersController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiHarjoituskoodi.Models;
+
+namespace WebApiHarjoituskoodi.Controllers
+{
+    [Route("nw/[controller]")]
+    [ApiController]
+    public class SuppliersController : ControllerBase
+    {
+        northwindContext db = new northwindContext();
+
+        //-------------------get all northwind Categories
+        [HttpGet]
+        [Route("")]
+        public List<Suppliers> GetAllCategories()
+        {
+            northwindContext db = new northwindContext();
+            List<Suppliers> suppliers = db.Suppliers.ToList();
+            db.Dispose();
+            return suppliers;
+        }
+    }
+}
